1661928afb505a4ff9e36cf3bd0813ed836db935
Hiep Nguyen <hiepnh610@gmail.com>
2021-09-17 08:59:54 +0700

    feat: update the config of tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
 import { ReactElement } from 'react';
 import { Button } from '@parley-hub/grace';
 
-import AuthLayout from '../layouts/auth';
+import GuessLayout from '../layouts/guess';
 
 const HomeTemplate = (): ReactElement => {
   return <Button label="Test" variant="success" />;
 };
 
 const Home = (): ReactElement => {
-  return <AuthLayout Component={HomeTemplate} />;
+  return <GuessLayout Component={HomeTemplate} />;
 };
 
 export default Home;
