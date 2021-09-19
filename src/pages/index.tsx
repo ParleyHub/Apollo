bc443dfbdff4e5a3f62b4533a8689fea62d9d8fe
Hiep Nguyen <hiepnh610@gmail.com>
2021-09-19 23:37:04 +0700

    feat: implement lazy loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from 'react';
+import LazyLoad from 'react-lazyload';
 import Image from 'next/image';
 
 import GuessLayout from '../layouts/guess';
@@ -40,162 +41,168 @@ const HomeTemplate = (): ReactElement => {
         </div>
       </div>
 
-      <div className="bg-white py-20">
-        <div className="container">
-          <div className="md:grid md:grid-cols-2 md:gap-12">
-            <div className="mb-8 md:order-2 md:mb-0">
-              <div className="grid grid-rows-2 grid-cols-12 gap-4">
-                <div className="col-span-7 row-span-2 relative">
-                  <Image
-                    src={image02}
-                    alt="Image 02"
-                    className="rounded-md"
-                    objectFit="cover"
-                    layout="responsive"
-                  />
-                </div>
+      <LazyLoad>
+        <div className="bg-white py-10 md:py-20">
+          <div className="container">
+            <div className="md:grid md:grid-cols-2 md:gap-12">
+              <div className="mb-8 md:order-2 md:mb-0">
+                <div className="grid grid-rows-2 grid-cols-12 gap-4">
+                  <div className="col-span-7 row-span-2 relative">
+                    <Image
+                      src={image02}
+                      alt="Image 02"
+                      className="rounded-md"
+                      objectFit="cover"
+                      layout="responsive"
+                    />
+                  </div>
 
-                <div className="col-span-5 row-span-1 relative">
-                  <Image
-                    src={image03}
-                    alt="Image 03"
-                    className="rounded-md"
-                    objectFit="cover"
-                    layout="fill"
-                  />
-                </div>
+                  <div className="col-span-5 row-span-1 relative">
+                    <Image
+                      src={image03}
+                      alt="Image 03"
+                      className="rounded-md"
+                      objectFit="cover"
+                      layout="fill"
+                    />
+                  </div>
 
-                <div className="col-span-5 row-span-1 relative">
-                  <Image
-                    src={image04}
-                    alt="Image 04"
-                    className="rounded-md"
-                    objectFit="cover"
-                    layout="fill"
-                  />
+                  <div className="col-span-5 row-span-1 relative">
+                    <Image
+                      src={image04}
+                      alt="Image 04"
+                      className="rounded-md"
+                      objectFit="cover"
+                      layout="fill"
+                    />
+                  </div>
                 </div>
+              </div>
+
+              <div className="md:order-1">
+                <h2 className="text-3xl lg:text-4xl mb-6 text-gray-800">
+                  Parley Hub is for everyone
+                </h2>
+
+                <p className="text-lg lg:text-xl mb-4 text-gray-800">
+                  Whether it’s chat, calls, or video, anyone can engage at any
+                  time, bringing everyone closer.
+                </p>
+
+                <p className="text-lg lg:text-xl mb-4 text-gray-800">
+                  Your docs, photos, videos, chat history, and meeting notes are
+                  always there, so it’s easier to work together.
+                </p>
+
+                <p className="text-lg lg:text-xl text-gray-800">
+                  Set up your team’s space with all the apps you need so you can
+                  stay in just one place instead of jumping around.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </LazyLoad>
+
+      <LazyLoad>
+        <div className="bg-gray-100 py-10 md:py-20">
+          <div className="container">
+            <div className="md:grid md:grid-cols-12 md:gap-4 mb-8">
+              <div className="md:col-span-6">
+                <h2 className="text-3xl lg:text-4xl mb-4 text-gray-900">
+                  Work more easily with everyone
+                </h2>
+
+                <p>
+                  Stay on the same page and make decisions faster by bringing
+                  all of your work communication into one place.
+                </p>
               </div>
             </div>
 
-            <div className="md:order-1">
-              <h2 className="text-3xl lg:text-4xl mb-6 text-gray-800">
-                Parley Hub is for everyone
-              </h2>
+            <div className="md:grid md:grid-rows-2 md:grid-cols-12 md:gap-4 items-center">
+              <div className="bg-white rounded-md mb-4 p-4 row-span-2 md:col-span-6 md:mb-0 lg:col-span-3">
+                <h4 className="text-xl font-medium mb-2 text-gray-900">
+                  Channels
+                </h4>
 
-              <p className="text-lg lg:text-xl mb-4 text-gray-800">
-                Whether it’s chat, calls, or video, anyone can engage at any
-                time, bringing everyone closer.
-              </p>
+                <p className="text-gray-600">
+                  Stay organized and focused with central spaces for
+                  conversations, files, tools and people.
+                </p>
+              </div>
 
-              <p className="text-lg lg:text-xl mb-4 text-gray-800">
-                Your docs, photos, videos, chat history, and meeting notes are
-                always there, so it’s easier to work together.
-              </p>
+              <div className="bg-white rounded-md mb-4 p-4 row-span-1 md:col-span-6 md:mb-0 lg:col-span-3">
+                <h4 className="text-xl font-medium text-gray-900">Message</h4>
+              </div>
 
-              <p className="text-lg lg:text-xl text-gray-800">
-                Set up your team’s space with all the apps you need so you can
-                stay in just one place instead of jumping around.
-              </p>
+              <div className="bg-white rounded-md mb-4 p-4 row-span-1 md:col-span-6 md:mb-0 lg:col-span-3">
+                <h4 className="text-xl font-medium text-gray-900">
+                  Voice and video calls
+                </h4>
+              </div>
+
+              <div className="bg-white rounded-md mb-4 p-4 row-span-1 md:col-span-6 md:mb-0 lg:col-span-3">
+                <h4 className="text-xl font-medium text-gray-900">
+                  Accessibility
+                </h4>
+              </div>
+
+              <div className="bg-white rounded-md mb-4 p-4 row-span-1 md:col-span-6 md:mb-0 lg:col-span-3">
+                <h4 className="text-xl font-medium text-gray-900">
+                  File sharing
+                </h4>
+              </div>
+
+              <div className="bg-white rounded-md mb-4 p-4 row-span-1 md:col-span-6 md:mb-0 lg:col-span-3">
+                <h4 className="text-xl font-medium text-gray-900">Security</h4>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </LazyLoad>
 
-      <div className="bg-gray-100 py-20">
-        <div className="container">
-          <div className="md:grid md:grid-cols-12 md:gap-4 mb-8">
-            <div className="md:col-span-6">
-              <h2 className="text-3xl lg:text-4xl mb-4 text-gray-900">
-                Work more easily with everyone
-              </h2>
-
-              <p>
-                Stay on the same page and make decisions faster by bringing all
-                of your work communication into one place.
-              </p>
-            </div>
-          </div>
-
-          <div className="md:grid md:grid-rows-2 md:grid-cols-12 md:gap-4 items-center">
-            <div className="bg-white rounded-md mb-4 p-4 row-span-2 md:col-span-6 md:mb-0 lg:col-span-3">
-              <h4 className="text-xl font-medium mb-2 text-gray-900">
-                Channels
-              </h4>
-
-              <p className="text-gray-600">
-                Stay organized and focused with central spaces for
-                conversations, files, tools and people.
-              </p>
+      <LazyLoad>
+        <div className="bg-white">
+          <div className="grid grid-cols-12 grid-rows-3 md:grid-rows-2">
+            <div className="relative col-span-12 md:col-span-6 md:row-span-2 md:block">
+              <Image
+                src={image05}
+                alt="Image 05"
+                objectFit="cover"
+                layout="fill"
+              />
             </div>
 
-            <div className="bg-white rounded-md mb-4 p-4 row-span-1 md:col-span-6 md:mb-0 lg:col-span-3">
-              <h4 className="text-xl font-medium text-gray-900">Message</h4>
+            <div className="relative col-span-12 md:col-span-3 md:row-span-1 md:block">
+              <Image
+                src={image06}
+                alt="Image 06"
+                objectFit="cover"
+                layout="fill"
+              />
             </div>
 
-            <div className="bg-white rounded-md mb-4 p-4 row-span-1 md:col-span-6 md:mb-0 lg:col-span-3">
-              <h4 className="text-xl font-medium text-gray-900">
-                Voice and video calls
-              </h4>
+            <div className="relative col-span-6 md:col-span-3 md:row-span-2">
+              <Image
+                src={image07}
+                alt="Image 07"
+                objectFit="cover"
+                layout="responsive"
+              />
             </div>
 
-            <div className="bg-white rounded-md mb-4 p-4 row-span-1 md:col-span-6 md:mb-0 lg:col-span-3">
-              <h4 className="text-xl font-medium text-gray-900">
-                Accessibility
-              </h4>
-            </div>
-
-            <div className="bg-white rounded-md mb-4 p-4 row-span-1 md:col-span-6 md:mb-0 lg:col-span-3">
-              <h4 className="text-xl font-medium text-gray-900">
-                File sharing
-              </h4>
-            </div>
-
-            <div className="bg-white rounded-md mb-4 p-4 row-span-1 md:col-span-6 md:mb-0 lg:col-span-3">
-              <h4 className="text-xl font-medium text-gray-900">Security</h4>
+            <div className="relative col-span-6 md:col-span-3 md:row-span-1">
+              <Image
+                src={image08}
+                alt="Image 08"
+                objectFit="cover"
+                layout="fill"
+              />
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="bg-white">
-        <div className="grid grid-cols-12 grid-rows-2">
-          <div className="relative col-span-12 sm:col-span-6 sm:row-span-2">
-            <Image
-              src={image05}
-              alt="Image 05"
-              objectFit="cover"
-              layout="fill"
-            />
-          </div>
-
-          <div className="relative col-span-6 sm:col-span-3 row-span-1">
-            <Image
-              src={image06}
-              alt="Image 06"
-              objectFit="cover"
-              layout="fill"
-            />
-          </div>
-
-          <div className="relative col-span-6 sm:col-span-3 row-span-2">
-            <Image
-              src={image07}
-              alt="Image 07"
-              objectFit="cover"
-              layout="responsive"
-            />
-          </div>
-
-          <div className="relative col-span-6 sm:col-span-3 row-span-1">
-            <Image
-              src={image08}
-              alt="Image 08"
-              objectFit="cover"
-              layout="fill"
-            />
-          </div>
-        </div>
-      </div>
+      </LazyLoad>
     </>
   );
 };
