1c0410afade5bd5aca2fad920950f51f8866975c
Hiep Nguyen <hiepnh610@gmail.com>
2021-09-19 17:30:54 +0700

    feat: update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ const HomeTemplate = (): ReactElement => {
         </div>
       </div>
 
-      <div className="bg-white py-28">
+      <div className="bg-white py-20">
         <div className="container">
           <div className="grid grid-cols-2 gap-12">
             <div>
@@ -51,26 +51,124 @@ const HomeTemplate = (): ReactElement => {
             </div>
 
             <div>
-              <div className="grid grid-cols-2 gap-4">
-                <img
-                  src="https://images.pexels.com/photos/3205403/pexels-photo-3205403.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                  alt=""
-                  className="row-span-2 object-cover"
-                />
+              <div className="grid grid-rows-2 grid-cols-12 gap-4">
+                <div className="col-span-7 row-span-2">
+                  <img
+                    src="https://images.pexels.com/photos/3205403/pexels-photo-3205403.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                    alt=""
+                    className="object-cover"
+                  />
+                </div>
 
-                <img
-                  src="https://images.pexels.com/photos/5700184/pexels-photo-5700184.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                  alt=""
-                  className="row-span-1 object-cover"
-                />
+                <div className="col-span-5 row-span-1">
+                  <img
+                    src="https://images.pexels.com/photos/5700184/pexels-photo-5700184.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                    alt=""
+                    className="object-cover h-full"
+                  />
+                </div>
 
-                <img
-                  src="https://images.pexels.com/photos/4492161/pexels-photo-4492161.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                  alt=""
-                  className="row-span-2 object-cover"
-                />
+                <div className="col-span-5 row-span-1">
+                  <img
+                    src="https://images.pexels.com/photos/4492161/pexels-photo-4492161.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                    alt=""
+                    className="object-cover h-full"
+                  />
+                </div>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-gray-100 py-20">
+        <div className="container">
+          <div className="grid grid-cols-12 gap-4 mb-8">
+            <div className="col-span-6">
+              <h2 className="text-4xl mb-4 text-gray-900">
+                Work more easily with everyone
+              </h2>
+
+              <p>
+                Stay on the same page and make decisions faster by bringing all
+                of your work communication into one place.
+              </p>
+            </div>
+          </div>
+
+          <div className="grid grid-rows-2 grid-cols-12 gap-4">
+            <div className="col-span-3 bg-white p-4 row-span-2">
+              <h4 className="text-xl font-medium mb-2 text-gray-900">
+                Channels
+              </h4>
+
+              <p className="text-gray-600">
+                Stay organized and focused with central spaces for
+                conversations, files, tools and people.
+              </p>
+            </div>
+
+            <div className="col-span-3 bg-white p-4 row-span-1">
+              <h4 className="text-xl font-medium text-gray-900">Message</h4>
+            </div>
+
+            <div className="col-span-3 bg-white p-4 row-span-1">
+              <h4 className="text-xl font-medium text-gray-900">
+                Voice and video calls
+              </h4>
+            </div>
+
+            <div className="col-span-3 bg-white p-4 row-span-1">
+              <h4 className="text-xl font-medium text-gray-900">
+                Accessibility
+              </h4>
+            </div>
+
+            <div className="col-span-3 bg-white p-4 row-span-1">
+              <h4 className="text-xl font-medium text-gray-900">
+                File sharing
+              </h4>
+            </div>
+
+            <div className="col-span-3 bg-white p-4 row-span-1">
+              <h4 className="text-xl font-medium text-gray-900">Security</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-white">
+        <div className="grid grid-cols-12 grid-rows-2">
+          <div className="col-span-6 row-span-2">
+            <img
+              src="https://images.pexels.com/photos/4492135/pexels-photo-4492135.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+              className="h-full object-cover"
+            />
+          </div>
+
+          <div className="col-span-3 row-span-1">
+            <img
+              src="https://images.pexels.com/photos/3783118/pexels-photo-3783118.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+              className="h-full object-cover"
+            />
+          </div>
+
+          <div className="col-span-3 row-span-2">
+            <img
+              src="https://images.pexels.com/photos/4246228/pexels-photo-4246228.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+              className="h-full object-cover"
+            />
+          </div>
+
+          <div className="col-span-3 row-span-1">
+            <img
+              src="https://images.pexels.com/photos/3782181/pexels-photo-3782181.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt=""
+              className="h-full object-cover"
+            />
           </div>
         </div>
       </div>
