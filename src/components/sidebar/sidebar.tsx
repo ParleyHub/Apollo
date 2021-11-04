b990d0c6c6ddcf61831005f9bbd477fa18035873
Hiep Nguyen <hiepnh610@gmail.com>
2021-11-04 13:24:11 +0700

    feat: install react query

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from 'react';
 
 const SidebarComponent = (): ReactElement => (
   <aside className="bg-white w-80 flex border-r border-solid border-gray-200 p-4">
-    <div className="font-medium">News Feed</div>
+    <div className="font-medium">Friends</div>
   </aside>
 );
 
